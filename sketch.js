26fc62c47aff9ff7a27da32fc10631cef6697f7a
kwong09 <wongkayla09@gmail.com>
2024-11-24 09:27:29 +0000

    added eggs and tested adding ingredients

--- sketch.js
+++ sketch.js
@@ -1,18 +1,79 @@
-let ball;
+let screen = 0;
+let homeScreenStart;
+let bowl, egg, sugar;
 
 function setup() {
-	new Canvas(500, 500);
+	new Canvas(1000, 500);
 	displayMode('centered');
+	background('white');
 
-	ball = new Sprite();
-	ball.diameter = 50;
+	homeScreenStart = new Sprite(500, 250, 100, 50, 's');
+	homeScreenStart.text = 'Start';
+	homeScreenStart.color = 'white';
+
+	bowl = new Sprite(-100, -100, 100, 50, 'k');
+	bowl.color = 'skyblue';
+
+	egg = new Sprite(-100, -100);
+	egg.image = 'https://kwong09.github.io/cupcake-baker/ingredients/egg0.png';
+	egg.scale = 0.07;
+	egg.diameter = 45;
+	egg.rotationLock = 'true';
+
+	sugar = new Sprite(-100, -100, 100, 50,);
+	sugar.color = 'purple';
+
 }
 
 function draw() {
-	background('skyblue');
+	if (screen == 0) {
+		clear();
+		background('skyblue');
 
-	if (mouse.presses()) {
-		ball.speed = 10;
-		ball.moveTo(mouse);
+		if (homeScreenStart.mouse.presses()) {
+			screen += 1;
+			homeScreenStart.pos = {x: -100, y: -100};
+
+			bowl.pos = {x: 500, y: 250};
+			egg.pos = {x: 700, y: 250};
+			sugar.pos = {x: 300, y: 250};
+		}
 	}
+
+	if (screen == 1) {
+		clear();
+		background('white');
+
+
+
+		if (egg.mouse.hovering() || sugar.mouse.hovering()) {
+			mouse.cursor = 'grab';
+		} else {
+			mouse.cursor = 'default';
+		}
+		
+		if (egg.mouse.dragging()) {
+			egg.x = mouse.x;
+			egg.y = mouse.y;
+		}
+
+		if (egg.overlaps(bowl)) {
+			egg.remove();
+		}
+
+
+		if (sugar.mouse.dragging()) {
+			sugar.x = mouse.x;
+			sugar.y = mouse.y;
+		}
+
+		if (sugar.overlaps(bowl)) {
+			sugar.remove();
+		}
+		
+		
+
+
+	}
+
 }
